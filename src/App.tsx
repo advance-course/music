e6e6efb8291816b4efeffbbc3c4ba321572e2790
yangbo <1832064870@qq.com>
2019-08-13 15:35:10 +0800

    初始化

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
+    <div className="app-container">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -20,7 +20,5 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
 }
-
-export default App;
